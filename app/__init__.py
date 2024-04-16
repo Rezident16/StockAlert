185e419af4218ae3fd3abd2e8aa0a50dea8ca187
Andrei Vorobev <andrey.vorob1995@gmail.com>
2024-04-16 15:12:44 -0400

    added user model

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask, render_template, request, session, redirect
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect, generate_csrf
+from flask_login import LoginManager
+from .config import Config
+
+app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
+
+app.config.from_object(Config)
